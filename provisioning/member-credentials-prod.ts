6edc0cda5441fe1f593c2f0596ee2833bec42d69
gazblair <gazblair@hotmail.com>
2022-04-02 12:57:18 +0000

    Initial Version

---
+++ provisioning/member-credentials-prod.ts
@@ -0,0 +1,13 @@
+
+import "source-map-support/register"
+import {App} from "@aws-cdk/core"
+import { MemberCredentials, StoreType } from "../infrastructure/member-credentials"
+
+const app = new App()
+new MemberCredentials(app, "MemberCredentialsProd", {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  stageName: "prod",
+  storeTypeForSettings: StoreType.Parameter
+})
+
+app.synth()
